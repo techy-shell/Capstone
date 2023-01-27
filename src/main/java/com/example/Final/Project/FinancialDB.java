42c685e1d2c068a0f11b53a469da9661a7a62512
smitjcb <dfghjk@>
2023-01-27 13:52:28 +0000

    FinancialDB testable api

--- src/main/java/com/example/Final/Project/FinancialDB.java
+++ src/main/java/com/example/Final/Project/FinancialDB.java
@@ -5,15 +5,19 @@ public class FinancialDB {
 
     //Getter and setters needed once names confirmed
     // name could change depending on the database.
-    private String userName;
+    private int userName;
     private int salary;
 
+    private int mortgage;
 
-    public String getUserName() {
+    private int bills;
+
+
+    public int getUserName() {
         return userName;
     }
 
-    public void setUserName(String userName) {
+    public void setUserName(int userName) {
         this.userName = userName;
     }
 
@@ -23,5 +27,21 @@ public class FinancialDB {
 
     public void setSalary(int salary) {
         this.salary = salary;
+    }
+
+    public int getMortgage() {
+        return mortgage;
+    }
+
+    public void setMortgage(int mortgage) {
+        this.mortgage = mortgage;
+    }
+
+    public int getBills() {
+        return bills;
+    }
+
+    public void setBills(int bills) {
+        this.bills = bills;
     }
 }
