9c2036650a50139a4adcfa83c6784ac09797afbc
ekjmca <ekjmcardle@gmail.com>
2023-01-30 20:10:18 +0000

    added input fields to income and expenditure tables, formatting needed aligned heading and links fields in line with homepage and help page

--- src/main/FrontEnd/Budget.js
+++ src/main/FrontEnd/Budget.js
@@ -1,18 +1,21 @@
-
+/*
 function getInfo() {
   var userName = document.getElementById("userChoice").value;
   fetch('localhost:8080/Financial/' + userName)
     .then(response => response.json())
     .then(response => populatePageCustomer(response));
+
+
 }
 
 function populatePageCustomer(response) {
 
     document.getElementById("userName").innerHTML = response.userName;
-    document.getElementById("salary").innerHTML = response.Financial.salary;
-    document.getElementById("mortgage").innerHTML = response.Financial.mortgage;
+    document.getElementById("salary").innerHTML = response.salary;
+    document.getElementById("mortgage").innerHTML = response.mortgage;
 
 }
+*/
 
 /*
 const url = 'http://localhost8080/Financial';
@@ -26,3 +29,12 @@ document.getElementById('sal').textContent = salary.toFixed(2);
 document.getElementById('mor').textContent = mortgage.toFixed(2);
 }
 */
+
+
+getText("localhost:8080/Financial");
+
+async function getText(url) {
+  let myObject = await fetch(url);
+  let myText = await myObject.text();
+  document.getElementById("demo").innerHTML = myText;
+}
