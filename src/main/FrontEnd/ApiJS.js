73fa7e18e8718ebe26e655e6ecbec9a583caaae6
KANABAK <karan6@hotmail.co.uk>
2023-01-31 14:55:10 +0000

    upvotes added

--- src/main/FrontEnd/ApiJS.js
+++ src/main/FrontEnd/ApiJS.js
@@ -1,12 +1,31 @@
 function getAllUsers() {
 //var newColor = document.getElementById('choice').value;
- fetch('localhost:8080/User')
+ fetch('http://localhost:8080/User')
  .then(response => response.json)
  .then(response => populate(response));
 //  const elem = document.getElementById('no1');
 //  elem.style.color = newColor;
 }
 function populate(response) {
-document.getElementById("no1").innerHTML = response.firstName;
+document.getElementById("no1").innerHTML = response;
 }
 
+//var likes = document.getElementById("like2").value;
+//var liked = false;
+//button = document.getElementById("LikeButton");
+//
+//function show() {
+//    button.innerHTML = likes + "like2";
+//}
+//
+//button.onclick = function() {
+//    if (liked == false) {
+//        liked = true;
+//        likes++;
+//        show();
+//    } else {
+//        liked = false;
+//        likes--;
+//        show();
+//    }
+//};
