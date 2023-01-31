cdc5a26fd19fe1eeb8b58dd626e0e1e4d4ae6135
Rachelle Maconeghy <rnmaconeghy@outlook.com>
2023-01-31 12:25:11 +0000

    updates to budget js

--- src/main/FrontEnd/Budget.js
+++ src/main/FrontEnd/Budget.js
@@ -1,11 +1,30 @@
 /*
-function getInfo() {
+Below lines of code work to print User data to console
+*/
+
+/*shows as Object object x10 on webpage*/
+ async function getUsers() {
+fetch('http://localhost:8080/User')
+  .then((response) => response.json())
+  .then((data) => {
+  document.getElementById('test').innerHTML = data;
+  console.log(data);
+  })};
+
+/*shows as undefined on webpage*/
+    async function getUsers2() {
+      let res = await fetch('http://localhost:8080/User);
+      res = await res.json();
+      users = res.users;
+      document.getElementById("test").innerHTML = users;
+      console.log(users);
+    }
+
+  /*
   var userName = document.getElementById("userChoice").value;
   fetch('localhost:8080/Financial/' + userName)
     .then(response => response.json())
     .then(response => populatePageCustomer(response));
-
-
 }
 
 function populatePageCustomer(response) {
