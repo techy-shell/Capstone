b5b8829e5f66f143e940da2d7edf242c7e07b425
KANABAK <karan6@hotmail.co.uk>
2023-01-31 10:22:19 +0000

    upvotes added

--- src/main/FrontEnd/ApiJS.js
+++ src/main/FrontEnd/ApiJS.js
@@ -1,4 +1,12 @@
-function getAllUsers{}
+function getAllUsers() {
+//var newColor = document.getElementById('choice').value;
+ fetch('localhost:8080/User')
+ .then(response => response.json)
+ .then(response => populate(response));
+//  const elem = document.getElementById('no1');
+//  elem.style.color = newColor;
+}
+function populate(response) {
+document.getElementById("no1").innerHTML = response.firstName;
+}
 
-
-https://localhost:8080/User
