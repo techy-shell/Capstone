bc8bda270f1a7a49b9a64e3c5ca0ee9c54c3f004
Rachelle Maconeghy <rnmaconeghy@outlook.com>
2023-01-31 16:03:37 +0000

    Local help API working with drop down menu

--- src/main/FrontEnd/LocalHelp.js
+++ src/main/FrontEnd/LocalHelp.js
@@ -9,21 +9,3 @@ fetch('http://localhost:8080/localhelp/' + cityID)
      })
      .catch(err => console.log(err));
      };
-
-/*let selects = document.querySelectorAll(".options select");
-let requestURL = "http://localhost:8080/LocalHelp/latest?cityID=Birmingham,Cornwall,Derby,Edinburgh,Leeds,Leicester,Liverpool,Manchester,Newcastle,Portsmouth"
-async function fetchHelp(){
-let res = await fetch(requestURL);
-res = await res.json();
-city = res.city;
-populateOptions();
-};
-
-function populateOptions(){
-let val = "";
-Object.keys(city).forEach(code => {
-let str = `<option value="${code}">${code}</option>`;
-val += str;
-});
-selects.forEach((s) => (s.innerHTML = val));
-};*/
