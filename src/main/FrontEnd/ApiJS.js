2b808b0d30408909c06605856a32b808a6cbd775
KANABAK <karan6@hotmail.co.uk>
2023-01-31 16:06:28 +0000

    js added

--- src/main/FrontEnd/ApiJS.js
+++ src/main/FrontEnd/ApiJS.js
@@ -1,31 +1,19 @@
-function getAllUsers() {
-//var newColor = document.getElementById('choice').value;
- fetch('http://localhost:8080/User')
- .then(response => response.json)
- .then(response => populate(response));
-//  const elem = document.getElementById('no1');
-//  elem.style.color = newColor;
-}
-function populate(response) {
-document.getElementById("no1").innerHTML = response;
+var likes = document.getElementById("like2").value;
+var liked = false;
+button = document.getElementById("LikeButton");
+
+function show() {
+    button.innerHTML = likes + "like2";
 }
 
-//var likes = document.getElementById("like2").value;
-//var liked = false;
-//button = document.getElementById("LikeButton");
-//
-//function show() {
-//    button.innerHTML = likes + "like2";
-//}
-//
-//button.onclick = function() {
-//    if (liked == false) {
-//        liked = true;
-//        likes++;
-//        show();
-//    } else {
-//        liked = false;
-//        likes--;
-//        show();
-//    }
-//};
+button.onclick = function() {
+    if (liked == false) {
+        liked = true;
+        likes++;
+        show();
+    } else {
+        liked = false;
+        likes--;
+        show();
+    }
+};
