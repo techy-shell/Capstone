01cfbd7cb101e1617e587ee9c20861bd2aca0f7f
Rachelle Maconeghy <rnmaconeghy@outlook.com>
2023-01-31 10:28:49 +0000

    Cross origin code added to Controllers to eliminate CORS issue

--- src/main/java/com/example/Final/Project/FinancialController.java
+++ src/main/java/com/example/Final/Project/FinancialController.java
@@ -1,6 +1,7 @@
 package com.example.Final.Project;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class FinancialController {
 
     private FinancialDAO financialDAO;
