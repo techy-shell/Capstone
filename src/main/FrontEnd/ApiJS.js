01a5e7c19c33231dccba2e8feeae3fa4241ed09e
Rachelle Maconeghy <rnmaconeghy@outlook.com>
2023-01-31 16:20:50 +0000

    callback request now working with API

--- src/main/FrontEnd/ApiJS.js
+++ src/main/FrontEnd/ApiJS.js
@@ -17,3 +17,14 @@ button.onclick = function() {
         show();
     }
 };
+
+ async function getUserInfo() {
+ var userID = document.getElementById('userID').value;
+fetch('http://localhost:8080/User/' + userID)
+  .then((response) => response.json())
+  .then((data) => {
+  console.log(data);
+    document.getElementById('callback').innerHTML = data[0].firstName + " " + data[0].surname + ", you will receive a callback within the next two working days on 0" + data[0].tel + ". You will receive an email confirmation for your callback at " + data[0].email + ".";
+     })
+     .catch(err => console.log(err));
+     };
