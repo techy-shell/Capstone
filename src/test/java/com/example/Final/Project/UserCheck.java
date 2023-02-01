e748744f9878e4ff9e71cc2e3f8ac00462b311c2
smitjcb <dfghjk@>
2023-02-01 15:16:34 +0000

    more testing

---
+++ src/test/java/com/example/Final/Project/UserCheck.java
@@ -0,0 +1,46 @@
+package com.example.Final.Project;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertEquals;
+
+public class UserCheck {
+
+//    Given user wants to check user/ user number name
+//    When passed username to chrome
+//    Then Chrome should open with correct users details
+
+    private UserCheck userCheck;
+
+    private WebDriver webDriver;
+
+    @Before
+    public void setUp(){
+        userCheck = new UserCheck();
+    }
+
+    @Given("user wants to check or use user name")
+    public void openBrowser (){
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+    }
+
+    @When("passed username to chrome")
+    public void userPassesInfo () {
+        webDriver.navigate().to("http://localhost:8080/User/100001");
+
+    }
+
+    @Then("Chrome should open with correct users details")
+    public void checkDetails (){
+        String text = webDriver.findElement(By.tagName("Body")).getText();
+        assertEquals("[{\"firstName\":\"Marco\",\"postcode\":\"DE1 0XW\",\"userID\":100001,\"surname\":\"Jordan\",\"email\":\"marcojordan@outlook.com\",\"tel\":\"7123123123\"}]",text);
+    }
+}
