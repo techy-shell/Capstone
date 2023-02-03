f45b0be4ea6c7e3da1cdb20a17884ff6edef6b23
Rachelle Maconeghy <rnmaconeghy@outlook.com>
2023-02-03 10:43:57 +0000

    comments added to budget.js

--- src/main/FrontEnd/Budget.js
+++ src/main/FrontEnd/Budget.js
@@ -1,5 +1,5 @@
 /*
- below function is to pull through financial information from DB using API
+ Below function is to pull through financial information from DB using API in the Income box
 */
 
  async function getUsers() {
@@ -15,7 +15,29 @@ fetch('http://localhost:8080/Financial/' + userID)
      };
 
 /*
-Below function is for expenditure calculation & pulls through API income data
+ Below function pushes updated info for income through to the Finances table in the DB using API
+*/
+
+function saveDetails() {
+var update = {
+'salary':document.getElementById("salary").value,
+'userName':document.getElementById("userName").value,
+'benefit':document.getElementById("benefits").value
+};
+
+fetch ('http://localhost:8080/update', {
+method: 'POST',
+headers: {
+'Content-Type':'application/json',
+},
+body: JSON.stringify(update)
+})
+.then(response => response.json())
+.then(response => console.log(JSON.stringify(response)));
+}
+
+/*
+Below function is for expenditure calculation & pulls through API income data in the Expenditure Box
 */
 
 function calcBudget() {
@@ -76,6 +98,10 @@ fetch('http://localhost:8080/TotalIncome/' + userID)
   document.getElementById("totalExp").innerHTML = "Total Expenditure - £" + total;
 }
 
+/*
+ Below function is for a callback option for the user to request a callback
+*/
+
 function selectOpt() {
          selEl = document.getElementById('topic');
          op = selEl.value;
@@ -95,32 +121,6 @@ function selectOpt() {
        };
 
 
-function saveDetails() {
-var update = {
-'salary':document.getElementById("salary").value,
-'userName':document.getElementById("userName").value,
-'benefit':document.getElementById("benefits").value
-};
 
-fetch ('http://localhost:8080/update', {
-method: 'POST',
-headers: {
-'Content-Type':'application/json',
-},
-body: JSON.stringify(update)
-})
-.then(response => response.json())
-.then(response => console.log(JSON.stringify(response)));
-}
 
-/*
-Positive remaining balance -
-Negative remaining balance -
-You may benefit from having a financial review to go over your current out goings to see if we can help reduce them. To book an appointment please complete the below callback request - *take same code from homepage & amend for financial review*
-You may want to consider one of the following lending options to help support you financially:
-*Undefined list options*
-Arranged overdraft - Our arranged overdraft allows you to go overdrawn up to an agreed limit, so it could help cover an unplanned expense. Think of it as a short-term safety net for your current account if you think you may go over on your monthly budget by a small amount and can afford it to pay it back. The interest rate on our arranged overdraft is 35% APR (variable) and a monthly of £5. You would only pay interest and the monthly charge if you use it. If you think this is the right option for you, it only takes about 5 minutes to apply. To apply or find out more about over Arranged overdrafts, please click here.
-Loan - A loan may be suitable if you think borrowing a fixed amount over a fixed period of time would be the best support for you. It could be helpful as you’ll know how long it will take to repay the loan and how much you need to pay back each month or if you are looking to consolidate debt or have a large one-off purchase to make. Our loan interest rates start at 4% APR and minimum loan amount is £2000. If you think this may be the right option for you or if you want to know more, please click here.
-Credit card - A credit card allows you to buy things up to an agreed limit and pay for them later. You could use it to make larger one off purchases and pay for them later, either in one go or in chunks. You can use a credit card to consolidate other credit or store card balances. Our Standard Rate credit card interest is 18% APR. If you would like to learn more aboubt our credit card or to apply for one, please click here.
-Mortgage review - If you have not had a mortgage review in the last 12 months, please consider having a review with one of our advisors. We could potentially save you money on your monthly mortgage payment, reduce your mortgage term or discuss the possibility to have a 3 month mortgage payment break. If you think this could be beneficial option for you, please complete the callback form below to book your mortgage review. - *take same code from homepage & amend for financial review*
-*/
+
